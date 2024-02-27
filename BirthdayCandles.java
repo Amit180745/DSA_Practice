0880b5edec43f16c9c40caf72ca0b8c922dfd6f5
Amit180745 <thakuramit9019@gmail.com>
2024-02-28 00:09:52 +0530

    DSA_Ques

--- BirthdayCandles.java
+++ BirthdayCandles.java
@@ -1,10 +1,33 @@
 package Practice_DSA;
 
+import java.util.*;
+
 public class BirthdayCandles {
+	
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        int max = 0;
+        for(int i = 0; i < arr.length ; i++)
+        {
+           int num = arr[i];
+           sc.close();
+           if(num > max)
+           {
+              max = num;
+              count = 1;
+           }else if(max == num)
+           {
+           count++;
+           }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+         }   
+         System.out.println(count);
+    }
 }
